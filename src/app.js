e853b22ece3f813b984c86d417316581a977fae5
GorgGoddess <jackson.anna1220@gmail.com>
2022-08-03 23:34:37 +0000

    Hi

--- src/app.js
+++ src/app.js
@@ -6,6 +6,16 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronoun = ["the", "our", "your"];
+  let adj = ["beautiful", "big", "little"];
+  let noun = ["house", "school", "park"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(`${pronoun[i]}${adj[j]}${noun[k]}.com`);
+      }
+    }
+  }
   console.log("Hello Rigo from the console!");
 };
